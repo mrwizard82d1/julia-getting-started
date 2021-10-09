65f108bde8d3379ea4e3157b508a4fde7604f47e
Larry Jones <larry.jones@reveal-energy.com>
2021-10-08 21:23:19 -0500

    Finished variables

--- pg02-variables.jl
+++ pg02-variables.jl
@@ -21,6 +21,7 @@ customary_phrase = "Hello, Julia World!"
 
 DeclarationOfIndependenceSelfEvidentTruths = "Wir halten diese Wahrheiten für selbstverständlich, dass alle Menschen gleich geschaffen sind, dass sie von ihrem Schöpfer mit bestimmen unveräußerlichen Rechten ausgestattet sind, darunter Leben, Freiheit and das Streben nach Glück."
 
+# Unicode names (in UTF-8 encoding) are allowed
 δ = 0.00001
 
 selbstverständlich = "self-evident"
@@ -42,3 +43,44 @@ pi = 3
 sqrt(100)
 
 sqrt = 4
+
+# Allowed variable names
+#
+# Variables start with many Unicode code points in specific categories:
+#
+# - Lu (uppercase letter)
+# - Ll (lowercase letter)
+# - Lt 
+# - Lm
+# - Lo
+# - Nl (letters (numbers))
+#
+# - Sc / So currency and other symbols
+# - A few other letter-like characters; for example, a subset of the Sm math symbols
+#
+# Subsequent characters include ! and digits (0-9 and other characters in categories Nd/No), as well as other
+# Unicode code points:
+# - Diacritics and other modifying marks (categories Mn/Mc/Me/Sk)
+# - Some punctuation connectors (category Pc)
+# - Primes
+# - A few other characters
+
+# Operators like `+` are also valid identifiers, but are parsed specially.
+
+# The only explicitly disallowed names for variables are the names of the built-in `keywords`:
+
+# else = false
+# try = "No"
+
+# Stylistic conventions for variable names
+#
+# - Names of variables are in lower case
+# - Word separation can be indicated by underscore ('_'), but the use of underscores is **discouraged** unless 
+#   the name would be hard to read otherwise
+# - Names of `Type`s and `Module`s begin with a capital letter and word separation is shown with upper camel case
+#   instead of underscores (PascalCase)
+# - Names of `functions` and `macros` are in lower case **without** underscores
+# - Functions that mutate their arguments have names that end in '!'. 
+#
+# For more information, see the Style Guide.
+
